39abec93b295ccabbbdd60370f387a47d661f619
Milton Chicas <chicasm@ferris.edu>
2017-11-20 13:13:37 -0500

    Added Logins for each of us, first initial Cap, lastname lowercase, password either 123456 or 654321. database logins. Logout button

--- CVGreenWare/Login_Form.cs
+++ CVGreenWare/Login_Form.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace CVGreenWare
 {
@@ -19,10 +20,64 @@ namespace CVGreenWare
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
-            AttemptLogin();
+            try
+            {
+
+                OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0; Data Source = |DataDirectory|\\WarehouseDatabase.accdb; Persist Security Info = True;");
+                OleDbCommand cmd = new OleDbCommand("Select * from tblUsers where UserName = '"+this.textBoxUsername.Text+"'AND Password='"+this.textBoxPassword.Text +"'AND Type;",con);
+                OleDbDataReader myReader;
+
+
+                con.Open();
+
+                myReader = cmd.ExecuteReader();
+
+                int count = 0;
+
+                while (myReader.Read())
+                {
+                    count = count + 1;
+                }
+                if (count == 1)
+                {
+                    
+                    if (textBoxUsername.Text == "Mchicas")
+                    {
+                        MessageBox.Show("Login Successful \nWelcome back, Milton");
+                                            }
+                    if (textBoxUsername.Text == "Bbuchan")
+                    {
+                        MessageBox.Show("Login Successful \nWelcome back, Brian");
+                    }
+                    if (textBoxUsername.Text == "Ascott")
+                    {
+                        MessageBox.Show("Login Successful \nWelcome back, Anthony");
+                    }
+                    if (textBoxUsername.Text == "Cwilson")
+                    {
+                        MessageBox.Show("Login Successful \nWelcome back, Chris");
+                    }
+
+                    AttemptSuccess();
+                }
+                else if (count > 1)
+                {
+                    MessageBox.Show("Invalid Username or Password");
+                }
+                else
+                    MessageBox.Show("Invalid Username or Password");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+                        
         }
 
-        private void AttemptLogin()
+
+
+        private void AttemptSuccess()
         {
             //test data
             Form1 program = new Form1();
